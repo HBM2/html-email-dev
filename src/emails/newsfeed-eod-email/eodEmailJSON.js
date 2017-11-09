0f8f8f5b7857f42bd63c5ec927a601050c638249
jeschaper <jschaper@hbm2.com>
2017-11-09 15:15:33 -0600

    Newsfeed EOD email

---
+++ src/emails/newsfeed-eod-email/eodEmailJSON.js
@@ -0,0 +1,72 @@
+{
+	HeaderDateText: 'FRIDAY AUGUST 25, 2017',
+	UnsubscribeLinkURL: '',
+	BubbleTemplate1: true,
+	BubbleTemplate2: false,
+	BubbleTemplate3: false,
+	BubbleTemplate4: false,
+	BubbleTemplate5: false,
+	BubbleTemplate6: false
+	Bubble1: {
+		Count: 13,
+		Text: 'NEW BUYERS',
+    BubbleLinkURL:'http://emailsAPI.hbm2.com/',
+		BuyerRegistration: true,
+		BuyerLogin: false,
+		BuyerViewingProperty: false,
+		BuyerFavoriteProperty: false,
+		BuyerRequestingFinancing: false,
+		BuyerScheduledShowing: false
+	},
+	Bubble2: {
+		...
+	},
+	Bubble3: {
+		...
+	},
+	Bubble4: {
+		...
+	},
+	Bubble5: {
+		...
+	},
+	Bubble6: {
+		...
+	}
+	ViewAllActivityLinkURL: 'http://emailsAPI.hbm2.com/....',
+	TopBuyers: [
+		{
+			BuyerName: 'Thomas Johnson',
+			ActivityCountText: '29 activities',
+			ViewMostRecentLinkURL: 'http://emailsAPI.hbm2.com/...',
+			Email: 'tjohnson@gmail.com',
+			PhoneNumber: '612-423-6449',
+			TopActivities: [
+				{
+					ActivityCount: 20,
+					LoggedInActivity: true,
+					SavedPropertyActivty: false,
+					ViewedPropertyActivity: false,
+					RequestedShowingActivity: false,
+					UsedCalculatorActivity: false,
+					SavedSearchActivity: false,
+					EditedSearchCriteriaActivity: false,
+					RequestedPreapprovalActivity: false,
+					RequestedCMAActivity: false,
+					ContactedTeamActivity: false,
+					CommentedOnPropertyActivity: false,
+					CreatedAccountActivity: false,
+				},
+				{
+					....
+				}
+			]
+		},
+		{
+			2nd top buyer ...
+		},
+		{
+			3rd top buyer...
+		}
+	]
+}
